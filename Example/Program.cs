3360001a15126e86c31f99050b03a295b96c43a3
Kiki <mullerkrisztianbusiness@gmail.com>
2023-11-19 16:25:40 +0100

    More example development.
    I believe there exist more bugs.

--- Example/Program.cs
+++ Example/Program.cs
@@ -7,9 +7,13 @@ namespace Example {
 
     internal static class Program {
 
-        static void ProcessText(StreamReader reader, Action<char> writeDelegate, double ratio, bool shouty) {
+        /// <summary>
+        /// 
+        /// </summary>
+        static void ProcessText(StreamReader reader, Action<char> writeDelegate, double ratio, double shoutiness, bool slow) {
 
-            bool inWhitespace = false;
+            bool shouty = shoutiness > 0;
+            char? lastCh = null;
             while(true) {
                 int next = reader.Read();
                 if(next == -1) break;
@@ -22,24 +26,62 @@ namespace Example {
                     ch = Random.Shared.NextDouble() <= 0.5 ? char.ToUpper(ch) : char.ToLower(ch);
                 }
 
-                if(shouty && !inWhitespace && char.IsWhiteSpace(ch)) {
-                    writeDelegate('!');
+                // Shout only if we're going from a letter to whitespace
+                if(shouty && lastCh.HasValue && char.IsLetter(lastCh.Value) && char.IsWhiteSpace(ch)) {
+                    while(Random.Shared.NextDouble() < shoutiness) {
+                        writeDelegate('!');
+                    }
                 }
 
-                inWhitespace = char.IsWhiteSpace(ch);
-
                 writeDelegate(ch);
+                lastCh = ch;
+
+                if(slow) System.Threading.Thread.Sleep(100);
             }
 
         }
 
 
         public static void Main( string[] args ) {
-            var ratioOpt = new Option(ValueExpectation.Optional, "r", "ratio");
-            var outOpt = new Option(ValueExpectation.Required, "o", "output");
-            var shoutyOpt = new Option(ValueExpectation.NotAllowed, "s", "shouty", "loud");
 
-            var options = new CommandLineOptions(args, new Option[] { ratioOpt, outOpt }, allowRedefinition: false);
+            var ratioOpt = new Option(ValueExpectation.Required, "r", "ratio");
+            var shoutyOpt = new Option(ValueExpectation.Optional, "s", "shouty", "loud");
+            var outOpt = new Option(ValueExpectation.Required, "o", "output");
+            var slowOpt = new Option(ValueExpectation.NotAllowed, "S", "slow");
+
+            var options = new CommandLineOptions(args, new Option[] { ratioOpt, shoutyOpt, outOpt, slowOpt }, allowRedefinition: false);
+
+            if(options.PositionalArguments.Count == 0) {
+                Console.WriteLine("Usage:\n\n[-r|--ratio=] [-s|--shouty=|--loud=] [-o|--output=] [-S|--slow] FILES");
+                return;
+            }
+
+
+            double ratio = 0.5;
+            double shoutiness = 0;
+            Action<char> writeDelegate = Console.Write;
+            bool slow = options.Options.ContainsKey(slowOpt);
+
+            if(options.Options.TryGetValue(ratioOpt, out string? ratioString)) ratio = double.Parse(ratioString! /* We can ! it, since it's required to have a value. */);
+            if(options.Options.TryGetValue(shoutyOpt, out string? shoutyString)) { // FIXME doesn't work without a value
+                if(shoutyString != null) shoutiness = double.Parse(shoutyString);
+                else shoutiness = 0.5;
+            }
+
+            StreamWriter? streamWriter = null;
+            if(options.Options.TryGetValue(outOpt, out string? outString)) {
+                streamWriter = new StreamWriter(File.Open(outString!, FileMode.OpenOrCreate, FileAccess.Write, FileShare.Delete));
+                writeDelegate = streamWriter.Write;
+            }
+
+            foreach(string posArg in options.PositionalArguments) {
+                using(var reader = new StreamReader(File.Open(posArg, FileMode.Open, FileAccess.Read, FileShare.Delete))) {
+                    ProcessText(reader, writeDelegate, ratio, shoutiness, slow);
+                }
+            }
+
+            streamWriter?.Dispose();
+
         }
 
     }
